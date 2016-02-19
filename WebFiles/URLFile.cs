7c91fb0bd927cb0bc02b5accd55c1d4c8ebf8914
q55x8x <q55x8x@yahoo.com>
2016-02-19 20:28:28 +0100

    Serializeable urlfile which stores all links

---
+++ WebFiles/URLFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaseAI_CE.WebFiles
+{
+    [Serializable]
+    public class URLFile
+    {
+        string avatarUrl;
+        private List<string> photoUrls = new List<string>();
+        private List<string> videoUrls = new List<string>();
+
+        public string AvatarUrl
+        {
+            get
+            {
+                return avatarUrl;
+            }
+
+            set
+            {
+                avatarUrl = value;
+            }
+        }
+
+        public List<string> PhotoUrls
+        {
+            get
+            {
+                return photoUrls;
+            }
+
+            set
+            {
+                photoUrls = value;
+            }
+        }
+
+        public List<string> VideoUrls
+        {
+            get
+            {
+                return videoUrls;
+            }
+
+            set
+            {
+                videoUrls = value;
+            }
+        }
+    }
+}
