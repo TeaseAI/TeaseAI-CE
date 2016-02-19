05d982657cb0d1ccccc48a15fff73345ab4e3ed9
q55x8x <q55x8x@yahoo.com>
2016-02-19 02:27:39 +0100

    Basic setup with working vlc control

---
+++ VLCTest/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vlc.DotNet.Core;
+using Vlc.DotNet.Forms;
+
+namespace VLCTest
+{
+    public partial class Form1 : Form
+    {
+        VlcControl vlcControl1;
+        Random rng = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).BeginInit();
+            // 
+            // vlcControl1
+            // 
+            this.vlcControl1.BackColor = System.Drawing.Color.Black;
+            this.vlcControl1.Location = new System.Drawing.Point(12, 112);
+            this.vlcControl1.Name = "vlcControl1";
+            this.vlcControl1.Size = new System.Drawing.Size(653, 344);
+            this.vlcControl1.Spu = -1;
+            this.vlcControl1.TabIndex = 1;
+            this.vlcControl1.Text = "vlcControl1";
+            this.vlcControl1.VlcLibDirectory = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\vlc");
+            this.vlcControl1.VlcMediaplayerOptions = null;
+
+            this.Controls.Add(this.vlcControl1);
+
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).EndInit();
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                vlcControl1.SetMedia(new FileInfo(ofd.FileName));
+                vlcControl1.Play();
+            }
+        }
+    }
+}
---
+++ VLCTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vlc.DotNet.Core;
+using Vlc.DotNet.Core.Interops;
+using Vlc.DotNet;
+using Vlc.DotNet.Forms;
+using Vlc.DotNet.Forms.TypeEditors;
+using Vlc;
+
+namespace VLCTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
