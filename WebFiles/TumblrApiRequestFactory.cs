22199f9452e3f6d562a2bc5880a3afab0ab29c24
q55x8x <q55x8x@yahoo.com>
2016-02-20 02:15:06 +0100

    - Command param was completely obsolete as my code only supports querying for blog data - Started adding documentation

--- WebFiles/TumblrApiRequestFactory.cs
+++ WebFiles/TumblrApiRequestFactory.cs
@@ -25,32 +25,62 @@ namespace TeaseAI_CE.WebFiles.TumblrAPI
         video
     }
 
+    /// <summary>
+    /// Class which is used to build TumblrApiRequests from a given TumblrBlog instance
+    /// </summary>
     public static class TumblrApiRequestFactory
     {
         const string APIKEY = "uWbnYYnVd2yLJRmuqwGXDCFH5RoX5nlvQwWdHRhqCsoM7W9Bqn";
 
+        /// <summary>
+        /// Build a request which queries for the blog info
+        /// </summary>
+        /// <param name="blog">Blog whose info is requested</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_Info(TumblrBlog blog)
         {
-            return BuildRequest(blog, Command.blog, Method.info);
+            return BuildRequest(blog, Method.info);
         }
 
+        /// <summary>
+        /// Build request which queries for the blog-avatar
+        /// </summary>
+        /// <param name="blog">Blog whose avatar is requested</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_Avatar(TumblrBlog blog)
         {
-            return BuildRequest(blog, Command.blog, Method.avatar);
+            return BuildRequest(blog, Method.avatar);
         }
 
         #region No type specified
 
+        /// <summary>
+        /// Builds a request which queries for the newest 20 posts
+        /// </summary>
+        /// <param name="blog">Blog whose posts are requested</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_Posts(TumblrBlog blog)
         {
-            return BuildRequest(blog, Command.blog, Method.posts);
+            return BuildRequest(blog, Method.posts);
         }
 
+        /// <summary>
+        /// Builds a request which queries for the first 20 posts starting from a given post offset
+        /// </summary>
+        /// <param name="blog">Blog whose posts are requested</param>
+        /// <param name="offset">Offset to to start from</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_PostsFrom(TumblrBlog blog, ulong offset)
         {
-            return BuildRequest(blog, Command.blog, Method.posts, new string[] { $"offset={offset}" });
+            return BuildRequest(blog, Method.posts, new string[] { $"offset={offset}" });
         }
 
+        /// <summary>
+        /// Builds a request which queries for a limited amount of posts starting from the newest post
+        /// </summary>
+        /// <param name="blog">Blog whose posts are requested</param>
+        /// <param name="limit">How many posts are queried for</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_PostsLimit(TumblrBlog blog, int limit)
         {
             if(limit > 20)
@@ -59,9 +89,16 @@ namespace TeaseAI_CE.WebFiles.TumblrAPI
             }
 
 
-            return BuildRequest(blog, Command.blog, Method.posts, new string[] { $"limit={limit}" });
+            return BuildRequest(blog, Method.posts, new string[] { $"limit={limit}" });
         }
 
+        /// <summary>
+        /// Builds a request which queries for a limited amount of posts starting from the offset given
+        /// </summary>
+        /// <param name="blog">Blog whose posts are requested</param>
+        /// <param name="offset">Offset to to start from</param>
+        /// <param name="limit">How many posts are queried for</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_PostsFromLimit(TumblrBlog blog, ulong offset, int limit)
         {
             if (limit > 20)
@@ -70,13 +107,19 @@ namespace TeaseAI_CE.WebFiles.TumblrAPI
             }
 
 
-            return BuildRequest(blog, Command.blog, Method.posts, new string[] { $"limit={limit}", $"offset={offset}" });
+            return BuildRequest(blog, Method.posts, new string[] { $"limit={limit}", $"offset={offset}" });
         }
 
         #endregion
 
         #region type specified
-       
+
+        /// <summary>
+        /// Builds a request which queries for the newest 20 posts of a specific type
+        /// </summary>
+        /// <param name="blog">Blog whose posts are requested</param>
+        /// <param name="postType">Type of posts which are requested</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_TypePosts(TumblrBlog blog, PostType postType)
         {
             string postType_str = "";
@@ -93,8 +136,16 @@ namespace TeaseAI_CE.WebFiles.TumblrAPI
                     throw new Exception("Invalid post type specified!");
             }
 
-            return BuildRequest(blog, Command.blog, Method.posts, new string[] { $"type={postType_str}" });
+            return BuildRequest(blog, Method.posts, new string[] { $"type={postType_str}" });
         }
+
+        /// <summary>
+        /// Builds a request which queries for the next 20 posts of a specific type
+        /// </summary>
+        /// <param name="blog">Blog whose posts are requested</param>
+        /// <param name="postType">Type of posts which are requested</param>
+        /// <param name="offset">Offset to to start from</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_TypePostsFrom(TumblrBlog blog, PostType postType, ulong offset)
         {
             string postType_str = "";
@@ -111,9 +162,16 @@ namespace TeaseAI_CE.WebFiles.TumblrAPI
                     throw new Exception("Invalid post type specified!");
             }
 
-            return BuildRequest(blog, Command.blog, Method.posts, new string[] { $"type={postType_str}", $"offset={offset}" });
+            return BuildRequest(blog, Method.posts, new string[] { $"type={postType_str}", $"offset={offset}" });
         }
 
+        /// <summary>
+        /// Builds a request which queries for a limited amount of posts of a specific type starting from the newest post
+        /// </summary>
+        /// <param name="blog">Blog whose posts are requested</param>
+        /// <param name="postType">Type of posts which are requested</param>
+        /// <param name="limit">How many posts are queried for</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_TypePostsLimit(TumblrBlog blog, PostType postType, int limit)
         {
             string postType_str = "";
@@ -136,9 +194,17 @@ namespace TeaseAI_CE.WebFiles.TumblrAPI
             }
 
 
-            return BuildRequest(blog, Command.blog, Method.posts, new string[] { $"type={postType_str}", $"limit={limit}" });
+            return BuildRequest(blog, Method.posts, new string[] { $"type={postType_str}", $"limit={limit}" });
         }
 
+        /// <summary>
+        /// Builds a request which queries for a limited amount of posts of a specific type starting from the given offset
+        /// </summary>
+        /// <param name="blog">Blog whose posts are requested</param>
+        /// <param name="postType">>Type of posts which are requested</param>
+        /// <param name="offset">Offset to to start from<</param>
+        /// <param name="limit">How many posts are queried for</param>
+        /// <returns></returns>
         public static TumblrApiRequest Request_TypePostsFromToLimit(TumblrBlog blog, PostType postType, ulong offset, int limit)
         {
             string postType_str = "";
@@ -161,32 +227,33 @@ namespace TeaseAI_CE.WebFiles.TumblrAPI
             }
 
 
-            return BuildRequest(blog, Command.blog, Method.posts, new string[] { $"type={postType_str}", $"limit={limit}", $"offset={offset}" });
+            return BuildRequest(blog, Method.posts, new string[] { $"type={postType_str}", $"limit={limit}", $"offset={offset}" });
         }
 
         #endregion
 
-        public static TumblrApiRequest BuildRequest(TumblrBlog blog, Command command, Method method)
+        /// <summary>
+        /// Builds a request which queries a given blog
+        /// </summary>
+        /// <param name="blog">Blog to query data from</param>
+        /// <param name="method">Method to query blog with</param>
+        /// <returns></returns>
+        public static TumblrApiRequest BuildRequest(TumblrBlog blog, Method method)
         {
-            return BuildRequest(blog, Command.blog, Method.posts, null);
+            return BuildRequest(blog, Method.posts, null);
         }
 
-        public static TumblrApiRequest BuildRequest(TumblrBlog blog, Command command, Method method, string[] options)
+        /// <summary>
+        /// Builds a request which queries a given blog
+        /// </summary>
+        /// <param name="blog">Blog to query data from</param>
+        /// <param name="method">Method to query blog with</param>
+        /// <param name="options">API specific options to query with</param>
+        /// <returns></returns>
+        public static TumblrApiRequest BuildRequest(TumblrBlog blog, Method method, string[] options)
         {
             string requestUrl = "api.tumblr.com/v2/";
-            string command_str = "";
             string method_str = "";
-
-            switch(command)
-            {
-                case Command.blog:
-                    command_str = "blog";
-                    break;
-                case Command.user:
-                    throw new NotImplementedException();                    
-                default:
-                    throw new Exception("Invalid command specified!");
-            }
 
             switch(method)
             {
@@ -203,7 +270,7 @@ namespace TeaseAI_CE.WebFiles.TumblrAPI
                     throw new Exception("Invalid method specified!");
             }
 
-            requestUrl += $"{command_str}/{blog.Url.Host}/{method_str}/?";
+            requestUrl += $"blog/{blog.Url.Host}/{method_str}/?";
 
             foreach(string option in options)
             {
