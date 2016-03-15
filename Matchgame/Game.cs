b2d53084299b37d40d81ecf82c8712df15898231
q55x8x <q55x8x@yahoo.com>
2016-03-15 14:41:56 +0100

    Game starting

--- Matchgame/Game.cs
+++ Matchgame/Game.cs
@@ -5,16 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Threading;
 
 namespace Matchgame
 {
     public class Game
     {
+        Thread GameRunner;
+
         Graphics g;
+
+        int xCardCount = 0;
+        int yCardCount = 0;
+
+        decimal cardHeight = 0;
+        decimal cardWidth = 0;
+
+        const int padding = 5;
+
+        public delegate void GameOverEventHandler(object sender);
+        event GameOverEventHandler GameOver;
+
+        Deck Cards = new Deck();
+
         public Game(PictureBox drawingArea, int pairCount)
         {
-            g = drawingArea.CreateGraphics();
-
             int uniqueCardCount = GameManager.Cards.Count;
 
             if(pairCount > uniqueCardCount)
@@ -22,7 +37,19 @@ namespace Matchgame
                 throw new Exception("Paircount exceeds cards present in the deck!");
             }
 
-            int cardcount = pairCount * 2;
+            GameManager.Cards.Shuffle();
+
+            for (int i = 0; i <= pairCount; i++)
+            {
+                Cards.Add(GameManager.Cards[i]);
+                Cards.Add(GameManager.Cards[i]);
+            }
+
+            Cards.Shuffle();
+
+            g = drawingArea.CreateGraphics();
+
+            int cardcount = Cards.Count;
 
             int x = 3;
             int y = 0;
@@ -36,7 +63,23 @@ namespace Matchgame
                 }
             }
 
-            GameManager.Cards.Shuffle();
+            xCardCount = x;
+            yCardCount = y;
+
+            cardHeight = (drawingArea.Height - ((y + 1) * padding)) / y; //strip complete height from all padding needed
+            cardWidth = (drawingArea.Width - ((x + 1) * padding)) / x;
+        }
+
+        public void Start()
+        {
+            foreach(Card card in Cards)
+            {
+                card.CardFlipped += Card_CardFlipped;
+            }
+        }
+
+        private void Card_CardFlipped(Card sender)
+        {
             
         }
     }
